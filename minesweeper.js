05fc370ce26550b970ac2e3998ef032bc124305c
wonkatin <j.drawers@gmail.com>
2019-01-02 19:43:13 -0500

    Game Board Function (8th Commit)

--- minesweeper.js
+++ minesweeper.js
@@ -9,9 +9,18 @@ console.log(guessLine);
 console.log(bombLine);
 console.log(blankLine);*/
 //CLEAN SLATE
+
+const printBoard = board => {
+  console.log("Current Board: ");
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
+
 let board = [
   [' ', ' ', ' '],
   [' ', ' ', ' '],
   [' ', ' ', ' ']
 ];
-console.log(board);
+
+printBoard(board);
