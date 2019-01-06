66d2b5773180ec7e4270a2ba8643852b1650c73f
wonkatin <j.drawers@gmail.com>
2019-01-05 20:03:54 -0500

    Generate Player Board Step 1 (11th Commit)

--- minesweeper.js
+++ minesweeper.js
@@ -31,3 +31,18 @@ board[2][2] = "B";
 
 printBoard(board);*/
 //CLEAN SLATE 2
+
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  const board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    const row = [];
+    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(' ');
+    }
+    board.push(row);
+  };
+  return board;
+
+
+};
+console.log(generatePlayerBoard(3,3));
